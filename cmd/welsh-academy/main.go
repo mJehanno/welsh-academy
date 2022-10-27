607d764b806a2cc8bded043a0622d1ca45d671b6
mjehanno <mathob.jehanno@hotmail.fr>
2022-10-27 09:48:37 +0200

    feat(ingredients): adds the possibility to create and list ingredients

--- cmd/welsh-academy/main.go
+++ cmd/welsh-academy/main.go
@@ -5,24 +5,30 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mjehanno/welsh-academy/pkg/ingredient"
 	"github.com/mjehanno/welsh-academy/pkg/user"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
+var userService *user.UserService
+var ingredientService *ingredient.IngredientService
 
-func main() {
+func init() {
 	var err error
-
 	dsn := "host=localhost user=nimda password=nimda dbname=welsh port=5432 sslmode=disable "
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	db.AutoMigrate(&user.User{})
 
 	if err != nil {
 		log.Fatalf("couldn't connect to database : %w", err)
 	}
+	db.AutoMigrate(&user.User{}, &ingredient.Ingredient{})
+}
+
+func main() {
+	userService = user.NewUserService(db)
+	ingredientService = ingredient.NewIngredientService(db)
 
 	r := gin.Default()
 	api := r.Group("/api")
@@ -33,6 +39,11 @@ func main() {
 			{
 				user.POST("/", createUserEndpoint)
 			}
+			ingredient := v1.Group("/ingredients")
+			{
+				ingredient.POST("/", createIngredientEndpoint)
+				ingredient.GET("/", getIngredientEndpoint)
+			}
 		}
 	}
 
@@ -40,8 +51,6 @@ func main() {
 }
 
 func createUserEndpoint(c *gin.Context) {
-	userService := user.NewUserService(db)
-
 	var json user.User
 
 	if err := c.ShouldBindJSON(&json); err != nil {
@@ -57,5 +66,31 @@ func createUserEndpoint(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"id": id,
 	})
+}
 
+func createIngredientEndpoint(c *gin.Context) {
+	var json ingredient.Ingredient
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := ingredientService.CreateIngredient(json)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": id,
+	})
+}
+
+func getIngredientEndpoint(c *gin.Context) {
+	ingredients, err := ingredientService.GetAllIngredient()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{})
+	}
+
+	c.JSON(http.StatusAccepted, ingredients)
 }
---
+++ pkg/ingredient/ingredient.go
@@ -0,0 +1,33 @@
+package ingredient
+
+import "gorm.io/gorm"
+
+type Ingredient struct {
+	gorm.Model
+	Name string
+}
+
+func NewIngredientService(db *gorm.DB) *IngredientService {
+	return &IngredientService{
+		db: db,
+	}
+}
+
+type IngredientService struct {
+	db *gorm.DB
+}
+
+func (is *IngredientService) CreateIngredient(ingredient Ingredient) (uint, error) {
+
+	result := is.db.Create(&ingredient)
+
+	return ingredient.ID, result.Error
+}
+
+func (is *IngredientService) GetAllIngredient() ([]Ingredient, error) {
+	var ingredients []Ingredient
+
+	result := is.db.Find(&ingredients)
+
+	return ingredients, result.Error
+}
